81f8051b3ba0a4136fefa5fdd2ed1bde3094a6b6
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-11-03 17:00:40 +0100

    Changed the device name to Cryo-Rec:LP:C4.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -4,4 +4,4 @@ require orkan
 
 epicsEnvSet ("IOCNAME", "ioc01-orkan")
 
-iocshLoad ("$(orkan_DIR)orkan.iocsh", "ASYN_PORT_NAME=PORT1,SERIAL_PORT=/dev/ttyUSB0,PREFIX=Cryo-Rec:LP:,DEV_NAME=Compr:")
+iocshLoad ("$(orkan_DIR)orkan.iocsh", "ASYN_PORT_NAME=PORT1,SERIAL_PORT=/dev/ttyUSB0,PREFIX=Cryo-Rec:LP:,DEV_NAME=C4:")
