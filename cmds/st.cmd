6fd4153c517dc715b2a316b19cb7179b31bdce35
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-08-04 15:32:51 +0200

    The first untested version of Modbus TCP.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,6 +2,11 @@
 
 require orkan
 
-epicsEnvSet ("IOCNAME", "ioc01-orkan")
+epicsEnvSet ("IOCNAME", "ioc23-orkan")
 
-iocshLoad ("$(orkan_DIR)orkan.iocsh", "ASYN_PORT_NAME=PORT1,SERIAL_PORT=/dev/ttyUSB0,PREFIX=Cryo-Rec:LP:,DEV_NAME=C4:")
+# Uncommet the line below for the serial port connection (ModBus RTU)
+#iocshLoad ("$(orkan_DIR)orkan.iocsh", "ASYN_PORT_NAME=PORT1,SERIAL_PORT=/dev/ttyUSB0,PREFIX=Cryo-Rec:LP:,DEV_NAME=C4:")
+
+# Connect using Modbus TCP
+iocshLoad ("$(orkan_DIR)orkan-tcp.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.39,PREFIX=Cryo-Rec:LP:,DEV_NAME=C4:,TOP=/opt/epics/autosave")
+
